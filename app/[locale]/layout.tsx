8900cb350d380ae0b1effc7c198106d0f213e593
asadapex <mrasad.apex@gmail.com>
2025-07-11 11:04:10 +0500

    language change

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,8 +1,11 @@
 import type { Metadata } from "next";
 import { Work_Sans } from "next/font/google";
-import "./globals.css";
+import "../globals.css";
 import Header from "@/modules/Header";
 import Footer from "@/modules/Footer";
+import { hasLocale, NextIntlClientProvider } from "next-intl";
+import { routing } from "@/i18n/routing";
+import { notFound } from "next/navigation";
 
 const workSans = Work_Sans({
   variable: "--font-work-sans",
@@ -19,21 +22,30 @@ export const metadata: Metadata = {
   description: "NFT Marketplace",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
-}: Readonly<{
+  params,
+}: {
   children: React.ReactNode;
-}>) {
+  params: Promise<{ locale: string }>;
+}) {
+  const { locale } = await params;
+  if (!hasLocale(routing.locales, locale)) {
+    notFound();
+  }
+
   return (
-    <html lang="en" className="h-full">
+    <html lang={locale} className="h-full">
       <body
         className={`${workSans.variable} ${workSansMono.variable} antialiased flex flex-col min-h-screen`}
       >
-        <main className="flex-grow">
-          <Header />
-          {children}
-        </main>
-        <Footer />
+        <NextIntlClientProvider>
+          <main className="flex-grow">
+            <Header />
+            {children}
+          </main>
+          <Footer />
+        </NextIntlClientProvider>
       </body>
     </html>
   );
--- modules/Header.tsx
+++ modules/Header.tsx
@@ -1,8 +1,27 @@
+"use client";
 import Button from "@/components/Button";
-import { Logo, UserIcon } from "@/public/icons";
+import { usePathname, useRouter } from "@/i18n/navigation";
+import { Logo, UserIcon, UsIcon } from "@/public/icons";
+import { useLocale, useTranslations } from "next-intl";
 import Link from "next/link";
+import { useTransition } from "react";
 
 const Header = () => {
+  const t = useTranslations("HeaderItems");
+  const router = useRouter();
+  const pathname = usePathname();
+  const locale = useLocale();
+  const [isPending, startTransition] = useTransition();
+
+  const handleLanguageChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedLocale = e.target.value;
+    document.cookie = `NEXT_LOCALE=${selectedLocale}; path=/`;
+
+    startTransition(() => {
+      router.replace(pathname, { locale: selectedLocale });
+    });
+  };
+
   return (
     <header className="flex justify-between items-center px-[50px] py-[27px]">
       <div>
@@ -12,16 +31,21 @@ const Header = () => {
       </div>
 
       <div className="flex items-center font-semibold text-white text-[16px] py-[12px] gap-[40px]">
-        <Link href="/marketplace">Marketplace</Link>
-        <Link href="/rankings">Rankings</Link>
-        <Link href="/connect-wall">Connect a wallet</Link>
+        <Link href="/marketplace">{t("NavItem1")}</Link>
+        <Link href="/rankings">{t("Navitem2")}</Link>
+        <Link href="/connect-wall">{t("Navitem3")}</Link>
         <Button
-          title="Sign up"
+          title={t("Button")}
           type="button"
           variant="filled"
           icon={<UserIcon />}
           iconPosition="left"
         />
+        <select onChange={handleLanguageChange} value={locale}>
+          <option value="en">EN</option>
+          <option value="ru">RU</option>
+          <option value="uz">UZ</option>
+        </select>
       </div>
     </header>
   );
